e4e71961d0a5110577deb1e678d6b288ed618246
deanpho <56141920+deanpho@users.noreply.github.com>
2022-07-25 14:47:47 -0400

    added a white space under public static bool FormIsOpen from Utils (for testing purpose)

--- CarsRentalApp/Utils.cs
+++ CarsRentalApp/Utils.cs
@@ -13,6 +13,7 @@ namespace CarsRentalApp
         // allows one instance (window) only
         public static bool FormIsOpen(string name)
         {
+
             // Check is widnow is already open
             var OpenForms = Application.OpenForms.Cast<Form>();
             var isOpen = OpenForms.Any(q => q.Name == name);
